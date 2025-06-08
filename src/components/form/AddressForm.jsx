c7c72404515081a2033a1a87b88b64cdba7d15c6
EduardoFelixNeto <felixsystems@hotmail.com>
2025-06-08 18:13:14 -0300

    refactor: rename address_cep to cep in AddressForm component

--- src/components/form/AddressForm.jsx
+++ src/components/form/AddressForm.jsx
@@ -38,7 +38,7 @@ export default function AddressForm({ register, errors, setValue, watch, control
                         CEP <span className="text-red-500">*</span>
                     </label>
                     <Controller
-                        name="address_cep"
+                        name="cep"
                         control={control}
                         render={({ field }) => {
                             let safeValue = field.value ?? '';
