4c946fdc52ad5e2012fd5bd33eb7c6328d245a93
felipebomfimdev <felipebomfimdev@gmail.com>
2025-07-07 22:53:49 -0300

    Home paciente finalizada

--- src/app/(admin)/evaluations/page.jsx
+++ src/app/(admin)/evaluations/page.jsx
@@ -1,9 +1,14 @@
 import EvaluationsContent from "@/components/pages/evaluations/EvaluationsContent";
+import {Suspense} from "react";
 
 export const metadata = {
     title: 'Avaliações | Equilibrium',
 };
 
 export default function EvaluationsPage() {
-    return <EvaluationsContent />;
+    return (
+        <Suspense fallback={<div>Carregando avaliações...</div>}>
+            <EvaluationsContent />
+        </Suspense>
+    );
 }
